2efcda431bbec786e48a6d812b951562a81f8092
nicodev <ndevaux@gmail.com>
2021-08-16 18:32:07 +0200

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
+  prefix: 'tw-',
   purge: ['./src/**/*.ejs', './src/**/*.md'],
   theme: {
     fontSize: {
@@ -15,28 +18,12 @@ module.exports = {
     },
     extend: {
       colors: {
-        blue: {
-          100: '#ebf8ff',
-          200: '#bee3f8',
-          300: '#90cdf4',
-          400: '#63b3ed',
-          500: '#4299e1',
-          600: '#3182ce',
-          700: '#2b6cb0',
-          800: '#2c5282',
-          900: '#2a4365',
-        },
-        gray: {
-          100: '#f7fafc',
-          200: '#edf2f7',
-          300: '#e2e8f0',
-          400: '#cbd5e0',
-          500: '#a0aec0',
-          600: '#718096',
-          700: '#4a5568',
-          800: '#2d3748',
-          900: '#1a202c',
-        },
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        green: colors.green,
+        yellow: colors.amber,
       },
     },
   },
